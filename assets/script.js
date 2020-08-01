cb1f41ef02945c635d3f7511c4a881bd17854bd8
Sr Komodo <komodosr@gmail.com>
2020-08-01 16:45:42 -0300

    use simpler title autohide instead of hamburger menu

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-const nav = document.getElementById("nav");
-
-if (window.matchMedia("(hover: none)").matches) {
-	nav.addEventListener("click", () => nav.classList.toggle("open"));
-} else {
-	nav.addEventListener("mouseenter", () => nav.classList.add("open"));
-}
-
-nav.addEventListener("mouseleave", () => nav.classList.remove("open"));
